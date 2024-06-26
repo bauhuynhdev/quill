535d2125832f30d92bac554dfbd9894527dffbbd
TOMO_Bau Huynh_BE/バウ/Keihi <bau.huynh@tomodev.vn>
2024-06-26 16:40:12 +0700

    - Refactor image.ts

--- packages/quill/src/formats/image.ts
+++ packages/quill/src/formats/image.ts
@@ -7,18 +7,18 @@ class Image extends EmbedBlot {
   static blotName = 'image';
   static tagName = 'IMG';
 
-    static create(value: string | { src: string; alt?: string }) {
-        const node = super.create() as HTMLImageElement;
-        if (typeof value === 'string') {
-            node.setAttribute('src', this.sanitize(value));
-        } else {
-            node.setAttribute('src', this.sanitize(value.src));
-            if (value.alt) {
-                node.setAttribute('alt', value.alt);
-            }
-        }
-        return node;
+  static create(value: string | { src: string; alt?: string }) {
+    const node = super.create() as HTMLImageElement;
+    if (typeof value === 'string') {
+      node.setAttribute('src', this.sanitize(value));
+    } else {
+      node.setAttribute('src', this.sanitize(value.src));
+      if (value.alt) {
+        node.setAttribute('alt', value.alt);
+      }
     }
+    return node;
+  }
 
   static formats(domNode: Element) {
     return ATTRIBUTES.reduce(
